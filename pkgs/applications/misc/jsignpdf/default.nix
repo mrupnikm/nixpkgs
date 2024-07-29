f6d94d16211c3a2fe90c4f0efba2ac756f0aa401
Matic Rupnik <matic.rupnik@marand.si>
2024-07-29 10:11:34 +0200

    there was an attempt v1

--- pkgs/applications/misc/jsignpdf/default.nix
+++ pkgs/applications/misc/jsignpdf/default.nix
@@ -1,14 +1,14 @@
 { lib, fetchFromGitHub, jre, makeWrapper, maven }:
 
 maven.buildMavenPackage rec {
-  pname = "jsignpdf";
-  version = "2.2.1";
+  pname = "JSignPdf";
+  version = "2_2_2";
 
   src = fetchFromGitHub {
     owner = "intoolswetrust";
     repo = pname;
-    rev = "${pname}-${version}";
-    #hash = "sha256-f467d42fa2fdd269e08e9f77fb3406ee167ef1548d9f4a0078f6401ee08e7224";
+    rev = "refs/tags/${pname}_${version}";
+    hash = "sha256-JUKPFemfdysRz/cWkX6Yr8SLuFCZwkd/IPcY1QcqTeo=";
   };
 
   #mvnHash = "sha256-kLpjMj05uC94/5vGMwMlFzLKNFOKeyNvq/vmB6pHTAo=";
@@ -21,3 +21,4 @@ maven.buildMavenPackage rec {
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ mrupnikm ];
   };
+}
